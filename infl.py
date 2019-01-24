1408f885bc7f392ed9f1bdf1ac9dee35c7007cb7
Krista Gurney <gurneykri@gmail.com>
2019-01-23 20:17:46 -0700

    inital commit

---
+++ infl.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+#######################################
+# module: infl.py
+# YOUR NAME
+# YOUR A#
+#######################################
+
+from const import const
+from deriv import deriv
+from poly12 import find_poly_1_zeros
+from tof import tof
+from point2d import point2d
+
+def find_infl_pnts(expr):
+    ## your code here
+    pass
+    
+    
+            
+
+    
---
+++ poly12.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+############################################
+# module: poly12.py
+# YOUR NAME
+# YOUR A#
+############################################
+
+from prod import prod
+from const import const
+from pwr import pwr
+from plus import plus
+from var import var
+from deriv import deriv
+from tof import tof
+import math
+
+def find_poly_1_zeros(expr):
+    ## your code here
+    pass
+
+def find_poly_2_zeros(expr):
+    ## your code here
+    pass
+
+    
+    
+    
+            
+    
+
+
+
+
